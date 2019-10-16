dba77c45c69a7a3a2e976dcf4a0baaa03092eb60
trinidadUTN <tveroonica@gmail.com>
2019-10-15 20:03:52 -0800

    Suma de Arrays

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace PonerEnUnArray10NumerosYMostrarlos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numeros = new int[10];
+
+            numeros[0] = 3;
+            numeros[1] = 5;
+            numeros[2] = 6;
+            numeros[3] = 14;
+            numeros[4] = 28;
+            numeros[5] = 39;
+            numeros[6] = 23;
+            numeros[7] = 32;
+            numeros[8] = 37;
+            numeros[9] = 398;
+           
+
+            int suma = numeros[6] + numeros[7];
+            System.Console.WriteLine(suma);
+
+            Console.ReadKey();
         }
     }
 }
