a572dfbc761cc969727f497a1ac82cb09852a3a2
trinidadUTN <tveroonica@gmail.com>
2019-10-15 19:54:09 -0800

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PonerEnUnArray10NumerosYMostrarlos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
