18b329dcc971bf27cdb93441b72eaa9571bab570
trinidadUTN <tveroonica@gmail.com>
2019-10-15 22:55:07 -0800

    PonerEnUnArray10Numeros

---
+++ PonerEnUnArray10NumerosYMostrarlos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PonerEnUnArray10NumerosYMostrarlos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[10];
+
+            numeros[0] = 3;
+            numeros[1] = 5;
+            numeros[2] = 6;
+            numeros[3] = 14;
+            numeros[4] = 28;
+            numeros[5] = 39;
+            numeros[6] = 23;
+            numeros[7] = 32;
+            numeros[8] = 37;
+            numeros[9] = 398;
+           
+
+            int suma = numeros[6] + numeros[7];
+            System.Console.WriteLine(suma);
+
+            Console.ReadKey();
+        }
+    }
+}
