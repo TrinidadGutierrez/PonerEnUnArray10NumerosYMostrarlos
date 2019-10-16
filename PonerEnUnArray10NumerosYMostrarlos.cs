7e144b30a81935bdc249eb1300aea280a548851d
trinidadUTN <tveroonica@gmail.com>
2019-10-15 22:58:36 -0800

    PonerEnUnArray10NumerosY

--- PonerEnUnArray10NumerosYMostrarlos.cs
+++ PonerEnUnArray10NumerosYMostrarlos.cs
@@ -7,7 +7,6 @@ namespace PonerEnUnArray10NumerosYMostrarlos
         static void Main(string[] args)
         {
             int[] numeros = new int[10];
-
             numeros[0] = 3;
             numeros[1] = 5;
             numeros[2] = 6;
@@ -18,11 +17,12 @@ namespace PonerEnUnArray10NumerosYMostrarlos
             numeros[7] = 32;
             numeros[8] = 37;
             numeros[9] = 398;
-           
 
-            int suma = numeros[6] + numeros[7];
-            System.Console.WriteLine(suma);
 
+            for (int contador = 0; contador < numeros.Length; contador++)
+            {
+                System.Console.WriteLine("El numero " + contador + "\t es igual a : " + numeros[contador]);
+            }
             Console.ReadKey();
         }
     }
